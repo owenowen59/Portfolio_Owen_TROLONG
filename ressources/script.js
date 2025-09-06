4866108f8c4e219a1d35e883d4295e17d6b1cd38
TROLONG <owen.trolong@gmail.com>
2025-09-06 22:55:54 +0200

    Ajout carroussel section projets intéressants

--- ressources/script.js
+++ ressources/script.js
@@ -296,7 +296,27 @@ if (searchInput) {
 }
 
 
+const carousel = document.querySelector(".carousel");
+const items = document.querySelectorAll(".carousel-item");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
+let currentIndex = 0;
+
+function updateCarousel() {
+  const offset = -currentIndex * 100;
+  carousel.style.transform = `translateX(${offset}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+});
 
 
 
